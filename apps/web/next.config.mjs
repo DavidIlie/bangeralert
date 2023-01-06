195d4ee86f3ee4fdf4788d7365e15d8a9ccf4aeb
David Ilie <david@davidilie.com>
2023-01-06 21:09:23 +0000

    env problems

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,17 +1,2 @@
 import withTM from "next-transpile-modules";
-
-/**
- * @template {import('next').NextConfig} T
- * @param {T} config - A generic parameter that flows through to the return type
- * @constraint {{import('next').NextConfig}}
- */
-function defineNextConfig(config) {
-   return config;
-}
-
-export default withTM(["@app/server"])(
-   defineNextConfig({
-      reactStrictMode: true,
-      swcMinify: true,
-   })
-);
+export default withTM(["@app/server"])();
