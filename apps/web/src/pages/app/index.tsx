eb3bb1d5d5437125f6f42975cbfcdc42325719a5
David Ilie <david@davidilie.com>
2023-01-10 10:11:41 +0000

    logo

--- apps/web/src/pages/app/index.tsx
+++ apps/web/src/pages/app/index.tsx
@@ -1,9 +1,7 @@
 import type { NextPage } from "next";
-import { useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
-   const { data } = useSession();
-   return <div>{data?.user?.name}</div>;
+   return <div></div>;
 };
 
 export default Home;
