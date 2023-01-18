915cb3e33daf9657d13fbbf07ae8beda73830163
David Ilie <david@davidilie.com>
2023-01-18 08:53:43 +0000

    reorder inputs

--- apps/expo/src/_app.tsx
+++ apps/expo/src/_app.tsx
@@ -1,6 +1,6 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { SessionProvider, useSession } from "next-auth/expo";
-import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
