1fa91d9e3362bd67873df04c360e244cc4991aa9
David Ilie <david@davidilie.com>
2023-01-23 14:13:40 +0000

    yay

--- apps/web/src/ui/panels/LeftPanel.tsx
+++ apps/web/src/ui/panels/LeftPanel.tsx
@@ -3,7 +3,8 @@ import * as React from "react";
 const LeftPanel: React.FC<{ small?: boolean }> = ({ small = false }) => {
   return (
     <div>
-      <h1 className="text-xl font-bold">People</h1>
+      <h1 className="mb-5 text-xl font-bold">People</h1>
+      <p className="font-medium text-gray-300">no friends:(</p>
     </div>
   );
 };
