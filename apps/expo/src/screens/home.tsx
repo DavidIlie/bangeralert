c1bd352d3dcf4af41621fed7066b9f09f5494c31
David Ilie <david@davidilie.com>
2023-02-27 18:20:24 +0000

    yep

--- apps/expo/src/screens/home.tsx
+++ apps/expo/src/screens/home.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-  SafeAreaView,
   FlatList,
   View,
   Image,
@@ -8,6 +7,7 @@ import {
   Linking,
   Text,
   ActivityIndicator,
+  SafeAreaView,
 } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
@@ -27,72 +27,70 @@ export const HomeScreen = () => {
   const feed = data?.pages.flatMap((page) => page.items.map((song) => song));
 
   return (
-    <SafeAreaView>
-      <FlatList
-        data={feed}
-        renderItem={({ item: song }) => (
-          <View className="mb-3 flex w-full gap-4 rounded-lg bg-dark-containers px-2 py-3">
-            <Pressable onPress={() => Linking.openURL(song.external_url)}>
-              <Image
-                source={{ uri: song.album[0]?.cover_url }}
-                className="h-30 w-30 rounded-md"
-              />
-            </Pressable>
+    <FlatList
+      data={feed}
+      renderItem={({ item: song }) => (
+        <SafeAreaView className="mx-auto mb-8 w-full flex-row gap-4 rounded-lg bg-dark-containers px-2 py-3">
+          <Pressable onPress={() => Linking.openURL(song.external_url)}>
+            <Image
+              source={{ uri: song.album[0]?.cover_url! }}
+              style={{ height: 150, width: 150, borderRadius: 5 }}
+            />
+          </Pressable>
+          <View>
             <View>
-              <View>
-                {song.name.length > 20 ? (
-                  <>
-                    <Text className="text-md truncate text-white">
-                      {song.name}
-                    </Text>
-                    <View className="flex items-center gap-0.5">
-                      <View className="flex">
-                        <Text className="font-xs text-gray-300">
-                          {song.album[0]?.name}
-                        </Text>
-                        <View className="mx-0.5" />
-                        <Text className="font-xs text-gray-300">
-                          {song.artist[0]?.name}
-                        </Text>
-                      </View>
-                      {song.explicit && (
-                        <MaterialIcons
-                          name="explicit"
-                          size={24}
-                          color="#d1d5db"
-                          className="mt-[0.05rem]"
-                        />
-                      )}
+              {song.name.length > 20 ? (
+                <>
+                  <Text className="text-md truncate text-white">
+                    {song.name}
+                  </Text>
+                  <View className="flex items-center gap-0.5">
+                    <View className="flex">
+                      <Text className="font-xs text-gray-300">
+                        {song.album[0]?.name}
+                      </Text>
+                      <View className="mx-0.5" />
+                      <Text className="font-xs text-gray-300">
+                        {song.artist[0]?.name}
+                      </Text>
                     </View>
-                  </>
-                ) : (
-                  <Text className="text-white">Hey</Text>
-                )}
-              </View>
+                    {song.explicit && (
+                      <MaterialIcons
+                        name="explicit"
+                        size={24}
+                        color="#d1d5db"
+                        className="mt-[0.05rem]"
+                      />
+                    )}
+                  </View>
+                </>
+              ) : (
+                <Text className="text-white">Hey</Text>
+              )}
             </View>
           </View>
-        )}
-        keyExtractor={(item) => item.id}
-        className="h-full"
-        onRefresh={() => {
-          utils.feed.getFeed.invalidate();
-        }}
-        refreshing={isLoading}
-        onEndReached={() => {
-          if (!isLoading && feed?.length !== 0) fetchNextPage();
-        }}
-        onEndReachedThreshold={0.5}
-        ListFooterComponent={() => {
-          if (isLoading) {
-            return (
-              <View style={{ paddingVertical: 20 }}>
-                <ActivityIndicator size="large" />
-              </View>
-            );
-          }
-          return null;
-        }}
-      />
-    </SafeAreaView>
+        </SafeAreaView>
+      )}
+      keyExtractor={(item) => item.id}
+      className="h-full"
+      onRefresh={() => {
+        utils.feed.getFeed.invalidate();
+      }}
+      refreshing={isLoading}
+      onEndReached={() => {
+        if (!isLoading && feed?.length !== 0) fetchNextPage();
+      }}
+      onEndReachedThreshold={0.5}
+      ListFooterComponent={() => {
+        if (isLoading) {
+          return (
+            <View style={{ paddingVertical: 20 }}>
+              <ActivityIndicator size="large" />
+            </View>
+          );
+        }
+        return null;
+      }}
+    />
   );
 };
